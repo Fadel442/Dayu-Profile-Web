3c9d76211951d7ac83906afb9f8ecb5e07b9104b
Fadel442 <fadelmuhammad4402@gmail.com>
2024-08-24 13:30:43 +0700

    commit 2

---
+++ resources/views/page/homepage.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.pagelay')
+
+@section('konten')
+    
+<h1 class="text">
+    Hello world!
+  </h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('page.homepage');
 });
+
+Route::get('/homepage', [PageController::class, 'homepages'])->name('homepage');
